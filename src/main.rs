f206f62076318472103f40b7dacf41dd218148fa
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-06 21:27:04 +0545

    feat: ✨ basic loop with exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+fn get_input(v: &mut String) {
+  io::stdin().read_line(v).expect("Failed to read input");
+  *v = v.trim().to_string()
+}
+
 fn main() {
-  println!("Hello, world!");
+  loop {
+    println!("What do you want to do?");
+    println!("7. quit app");
+    println!();
+
+    let mut selection: String = String::new();
+    get_input(&mut selection);
+    match selection.to_lowercase().as_str() {
+      "q" | "7" | "quit" | "exit" => break,
+      _ => println!("not valid command"),
+    }
+  }
 }
