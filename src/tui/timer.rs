43d9f608820b961a871dc2bcfcba28819503e438
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-09 16:48:19 +0545

    refactor: ♻️ better style for gauge

--- src/tui/timer.rs
+++ src/tui/timer.rs
@@ -5,21 +5,24 @@ use ratatui::{
   Frame,
   layout::{Constraint, Layout, Rect},
   style::Modifier,
-  widgets::{Gauge, Paragraph},
+  widgets::{LineGauge, Paragraph},
 };
 
 pub fn show_timer(timer_state: &TimerState, area: Rect, frame: &mut Frame) {
-  let layout = Layout::vertical([Constraint::Length(20), Constraint::Max(2)]).spacing(2);
+  let layout = Layout::vertical([Constraint::Length(20), Constraint::Max(1)]).spacing(2);
   let [first, second] = area.layout(&layout);
   let percent = (timer_state.time_remaining.as_secs()) as f64
-    / (time_for_session(timer_state.session_type).as_secs()) as f64
-    * 100_f64;
+    / (time_for_session(timer_state.session_type).as_secs()) as f64;
   let t = render_time(timer_state);
   let font = FIGlet::from_content(include_str!("../../resources/terminus.flf")).unwrap();
   let text = font.convert(&t).unwrap().to_string();
   frame.render_widget(Paragraph::new(text).centered(), first);
-  let progress = Gauge::default()
+  let progress = LineGauge::default()
     .style(Modifier::BOLD)
-    .percent((100_f64 - percent) as u16);
+    .filled_symbol("█")
+    .unfilled_symbol("░")
+    .label("")
+    .ratio(1_f64 - percent);
+
   frame.render_widget(progress, second);
 }
