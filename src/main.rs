7eda408bc7e6281e48821a299af8ba24171bf9b2
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-10 14:10:13 +0545

    feat(cmd): ✨ arguments to `pause`,`resume`,`toggle`,`start`,`skip` session

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ use std::{env, io};
 
 use crate::{
   config::settings::create_config_file,
-  daemon::run::{ensure_daemon_active, run_daemon},
+  daemon::{
+    commands::{Message, send_command},
+    run::{ensure_daemon_active, run_daemon},
+  },
   stats::calculate::print_stats,
   tui::app::Pages,
   utils::ignore::Ignore,
@@ -30,15 +33,27 @@ fn main() -> io::Result<()> {
   let lunch_tui = |page| tui::app::main(page);
   if args.len() > 1 {
     match args[1].as_str() {
+      // Commands to print stuff
       "print-stats" => {
         print_stats();
         Ok(())
       }
+
+      // Commands to open TUI in specific pages
       "tui" | "timer" | "home" => lunch_tui(Pages::Timer),
       "stats" | "stat" => lunch_tui(Pages::Stats),
       "history" => lunch_tui(Pages::History),
       "settings" | "config" => lunch_tui(Pages::Settings),
-      // TODO: Other commands like pause/play/resume to control when playing from daemon
+
+      // Commands to change state of timer
+      "pause" | "pause-session" => send_command(Message::PauseSession).map(|m| println!("{m}")),
+      "resume" | "resume-session" => send_command(Message::ResumeSession).map(|m| println!("{m}")),
+      "toggle" | "toggle-session" => send_command(Message::ToggleSession).map(|m| println!("{m}")),
+      "next" | "next-session" | "skip" | "skip-session" => {
+        send_command(Message::NextSession).map(|m| println!("{m}"))
+      }
+      "start" | "start-session" => send_command(Message::StartSession).map(|m| println!("{m}")),
+
       _ => lunch_tui(Pages::Timer),
     }
   } else {
