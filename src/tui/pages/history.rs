a3323332341756b1da6c1e0d86c2d4e97aa3042a
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-11 21:12:34 +0545

    fix(TUI): 🐛 history scroll

--- src/tui/pages/history.rs
+++ src/tui/pages/history.rs
@@ -1,5 +1,3 @@
-use std::sync::Mutex;
-
 use crate::{
   database::history::get_full_history_no_err, timer::utils::name_for_session, tui::app::AppState,
 };
@@ -8,17 +6,19 @@ use ratatui::{
   layout::{Constraint, Rect},
   widgets::{Paragraph, Row, Table},
 };
+use std::cmp::min;
+use std::sync::Mutex;
 
 static START_INDEX: Mutex<usize> = Mutex::new(0);
 
 pub fn scroll_history_down() {
   let mut n = START_INDEX.lock().unwrap();
-  *n += 1;
+  *n = n.saturating_add(1);
 }
 
 pub fn scroll_history_up() {
   let mut n = START_INDEX.lock().unwrap();
-  *n -= 1;
+  *n = n.saturating_sub(1);
 }
 
 pub fn get_history_index() -> usize {
@@ -35,16 +35,16 @@ pub fn show_history(area: Rect, frame: &mut Frame, app_state: &mut AppState) {
   let table_heading = Row::new(["Date", "Type", "Duration (mins)", "Status"]);
 
   let row_available = area.height as usize - 2;
-  let mut n = row_available;
+  let start = min(get_history_index(), app_state.max_history_index);
+  let n = min(start + row_available, all_history.len());
   if all_history.len() > row_available {
     app_state.max_history_index = all_history.len() - row_available
   } else {
-    n = all_history.len();
     app_state.max_history_index = 0
   }
 
   let mut table_items: Vec<Row> = Vec::new();
-  for i in get_history_index()..n {
+  for i in start..n {
     let history = &all_history[i];
     let status = if history.completed_duration == history.planned_duration {
       "Completed"
