a7b5bf1b9d9270dd2f0fd76f2b7a719929592a1c
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-11 23:58:47 +0545

    feat(stats): ✨ streak calculation

--- src/stats/calculate.rs
+++ src/stats/calculate.rs
@@ -5,10 +5,11 @@
 // - [x] Calculate total focus time
 // - [x] Calculate completed sessions
 // - [x] Calculate completion rate
-// - [ ] Calculate current streak
+// - [x] Calculate current streak
 // - [ ] Calculate longest streak
 
-use crate::database::history::get_db;
+use crate::database::history::{HistoryEntry, get_db, get_full_history_no_err};
+use chrono::{Duration, Local};
 
 #[derive(PartialEq)]
 pub enum DurType {
@@ -67,6 +68,25 @@ fn get_completion_rate() -> f32 {
   db.query_row("SELECT 100.0 * (SELECT COUNT(*) FROM history WHERE session_type='Work' AND completed_duration==planned_duration) / (SELECT COUNT(*) FROM history WHERE session_type='Work')", [], |row| row.get(0)).unwrap_or(0_f32)
 }
 
+fn get_current_streak(all_history: Vec<HistoryEntry>) -> i32 {
+  let mut streak = 0;
+  let mut expected_day = Local::now().date_naive();
+
+  // All history should sorted from newest to oldest
+  for history in all_history {
+    let day = history.end_time.date_naive();
+
+    if day == expected_day {
+      streak += 1;
+      expected_day -= Duration::days(1);
+    } else if day < expected_day {
+      break;
+    }
+  }
+
+  streak
+}
+
 pub fn print_stats() {
   println!("Total focused {} mins", get_total_time(DurType::All));
   println!(
@@ -83,4 +103,7 @@ pub fn print_stats() {
   );
   println!("Completted sessions {}", get_completed_sessions());
   println!("Completion rate {}%", get_completion_rate());
+
+  let history = get_full_history_no_err();
+  println!("Current streek {}", get_current_streak(history));
 }
