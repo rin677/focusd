ee4884e8081bdf31a1b59fbaae4ca600ef3a9c89
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-14 16:58:51 +0545

    fix: 🐛 different socket for dev build

--- src/daemon/mod.rs
+++ src/daemon/mod.rs
@@ -1,4 +1,8 @@
 pub mod commands;
 pub mod run;
 
-pub const SOCKET_PATH: &str = "/tmp/focusd.sock";
+pub const SOCKET_PATH: &str = if cfg!(feature = "dev-build") {
+  "/tmp/focusd-dev.sock"
+} else {
+  "/tmp/focusd.sock"
+};
