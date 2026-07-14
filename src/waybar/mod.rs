f80cef676a9fe2940358b1071607184295a6f98d
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-14 12:37:25 +0545

    feat(waybar): ✨ better tooltip and percentage fixed

--- src/waybar/mod.rs
+++ src/waybar/mod.rs
@@ -1,12 +1,14 @@
 use crate::{
   config::settings::get_crr_preset,
   daemon::commands::get_timer_state,
+  database::history::get_full_history_no_err,
+  stats::calculate::{DurType, get_completed_sessions, get_current_streak, get_total_time},
   timer::{
     engine::render_time,
     state::SessionType,
-    utils::{name_for_session, time_for_session},
+    utils::{name_for_session, next_session, time_for_session},
   },
-  utils::ignore::Ignore,
+  utils::{ignore::Ignore, times_ago::render_duration},
 };
 use serde::{Deserialize, Serialize};
 
@@ -36,17 +38,30 @@ pub fn status() {
   }
   let preset = get_crr_preset();
 
-  let icon = if state.running { "" } else { "" };
-
   let total_time = time_for_session(state.session_type);
-  let percentage = 100_f64 * state.time_remaining.as_secs() as f64 / total_time.as_secs() as f64;
+  let percentage =
+    100.0 - (100_f64 * state.time_remaining.as_secs() as f64 / total_time.as_secs() as f64);
+
+  let current_session = name_for_session(state.session_type);
+  let session_n = state.session_number;
+  let total_sessions = preset.sessions_before_long_break;
+  let next_session = next_session(state.session_type, state.session_number);
+  let next_session_name = name_for_session(next_session);
+  let time_for_next_sessoin = render_duration(time_for_session(next_session).as_secs());
+  let completed_today = render_duration(get_total_time(DurType::Today));
+  let history = get_full_history_no_err();
+  let current_streak = get_current_streak(history);
+  let streak_unit = if current_streak <= 1 { "day" } else { "days" };
 
   let tooltip = format!(
-    "{} {}/{} \n {icon} {}",
-    name_for_session(state.session_type),
-    state.session_number,
-    preset.sessions_before_long_break,
-    text
+    "{current_session} Session,
+Cycle {session_n}/{total_sessions}
+
+Next Session: {next_session_name} ({time_for_next_sessoin}),
+
+Focused today: {completed_today},
+Current Streak: {current_streak} {streak_unit}
+"
   );
 
   let module = WaybarModule {
