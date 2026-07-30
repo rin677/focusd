fb9062b185be50c85456025d326020a76aeeecd6
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-30 12:39:24 +0545

    feat(TUI): ✨ basic rendering of settings page

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -172,11 +172,13 @@ impl App {
       KeyCode::Char('j') | KeyCode::Down => match self.app_state.current_page {
         Pages::Timer => self.app_state.timer_page.preset_down(),
         Pages::History => self.app_state.history_page.history_down(),
+        Pages::Settings => self.app_state.settings_page.down(),
         _ => {}
       },
       KeyCode::Char('k') | KeyCode::Up => match self.app_state.current_page {
         Pages::Timer => self.app_state.timer_page.preset_up(),
         Pages::History => self.app_state.history_page.history_up(),
+        Pages::Settings => self.app_state.settings_page.up(),
         _ => {}
       },
       KeyCode::Enter => {
--- src/tui/pages/settings.rs
+++ src/tui/pages/settings.rs
@@ -1,17 +1,79 @@
-use crate::utils::figlet::big_text;
-use ratatui::{Frame, layout::Rect, widgets::Paragraph};
+use ratatui::{Frame, layout::Rect, prelude::*};
+use tui_widget_list::{ListBuilder, ListState, ListView};
 
-// TODO: Implement this
-#[allow(dead_code)]
-#[derive(Default)]
+pub enum SettingsItem {
+  Theme,
+  Notification,
+  Font,
+  DailyGoal,
+  Preset,
+  Hooks,
+  Sounds,
+}
+
+fn name_for_settings_item(item: &SettingsItem) -> String {
+  match item {
+    SettingsItem::Theme => "Theme".to_string(),
+    SettingsItem::Notification => "Notification".to_string(),
+    SettingsItem::Font => "Font".to_string(),
+    SettingsItem::DailyGoal => "DailyGoal".to_string(),
+    SettingsItem::Preset => "Preset".to_string(),
+    SettingsItem::Hooks => "Hooks".to_string(),
+    SettingsItem::Sounds => "Sounds".to_string(),
+  }
+}
+
 pub struct SettingsPage {
   pub selected_setting_index: usize,
+  pub settings_items: Vec<SettingsItem>,
+}
+
+impl Default for SettingsPage {
+  fn default() -> Self {
+    Self {
+      selected_setting_index: 0,
+      settings_items: vec![
+        SettingsItem::Theme,
+        SettingsItem::Notification,
+        SettingsItem::Font,
+        SettingsItem::DailyGoal,
+        SettingsItem::Preset,
+        SettingsItem::Hooks,
+        SettingsItem::Sounds,
+      ],
+    }
+  }
 }
 
 impl SettingsPage {
   pub fn render(&self, area: Rect, frame: &mut Frame) {
-    let text = big_text("Settings will be here");
-    let widget = Paragraph::new(text).centered();
-    frame.render_widget(widget, area);
+    let builder = ListBuilder::new(|context| {
+      let text = match self.settings_items.get(context.index) {
+        Some(item) => name_for_settings_item(item),
+        None => String::new(),
+      };
+      let mut item = Line::from(text);
+      if context.is_selected {
+        item = item.style(Style::default().bg(Color::DarkGray));
+      }
+      (item, 1)
+    });
+
+    let mut state = ListState::default();
+    state.select(Some(self.selected_setting_index));
+    let list = ListView::new(builder, self.settings_items.len());
+    list.render(area, frame.buffer_mut(), &mut state);
+  }
+
+  pub fn up(&mut self) {
+    if self.selected_setting_index > 0 {
+      self.selected_setting_index -= 1;
+    }
+  }
+  pub fn down(&mut self) {
+    let count = self.settings_items.len() - 1;
+    if self.selected_setting_index < count {
+      self.selected_setting_index += 1;
+    }
   }
 }
